800a7286e8b695bbb6f6f64f2090f9972b660838
jaya <avulajayanandareddy@gmail.com>
2024-07-02 17:59:03 +0530

    if else V1.0

---
+++ if-else.py
@@ -0,0 +1,13 @@
+n1=int(input("Enter number 1"))
+n2=int(input("Enter number 2"))
+
+if(n1>n2):
+    {
+        print("\n Number is Big",n1)
+    }
+else:
+    {
+    print("\n Number 2 is Big",n2)
+    }
+    
+    if(a>b and )
---
+++ user-input.py
@@ -0,0 +1,4 @@
+number=int(input("Enter Any First Number"))
+number2=int(input("Enter Any 2nd Number"))
+number3=number+number2
+print(number3)
